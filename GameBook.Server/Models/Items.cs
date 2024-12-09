6c2eab6f662b07091499177d3ef7fbe509c94868
L0SPAVL0S <pavel.horacek.022@pslib.cz>
2024-12-09 18:06:32 +0100

    Založení databáze

---
+++ GameBook.Server/Models/Items.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class Items
+{
+    [Key]
+    public int ItemID { get; set; }
+    public required string Name { get; set; }
+
+    public int? Value { get; set; }
+
+    public bool Stackable { get; set; }
+
+    public int CategoryId { get; set; }
+}
+
+public class ItemCategories
+{
+    [Key]
+    public int CategoryID { get; set; }
+    public string Name { get; set; }
+}
+
+
+public class ConsumableItems
+{
+    [Key]
+    public int ConsumableItemID { get; set; }
+
+    [ForeignKey(nameof(Items))]
+    public int ItemID { get; set; }
+    public int HealthValue { get; set; }
+    public int EnergyValue { get; set; }
+    public int RadiationValue { get; set; }
+}
--- GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
+++ GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1c2f13427dd88e371424874aac08be57bfd7844")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
