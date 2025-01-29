c41486119a8138b674b285df9cf8e177f2799e4a
Jakub Šajer <jakub.sajer@gmail.com>
2025-01-29 11:00:51 +0100

    ConsumableItem GET controller

--- GameBook.Server/Controllers/DataConsumableItemController.cs
+++ GameBook.Server/Controllers/DataConsumableItemController.cs
@@ -7,30 +7,79 @@ namespace GameBook.Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class DataConsumableItemController : ControllerBase
+    public class ConsumableItemController : ControllerBase
     {
         private AppDbContext _context;
-        public DataConsumableItemController(AppDbContext context)
+        public ConsumableItemController(AppDbContext context)
         {
             _context = context;
         }
 
         [HttpGet]
-        public ActionResult<IEnumerable<DataConsumableItem>> Get()
+        public ActionResult<IEnumerable<ViewConsumableItem>> Get()
         {
-            return Ok(_context.ConsumableItem.ToList());
+            var consumableItems = _context.ConsumableItem
+                .Include(ci => ci.Item)
+                .Select(ci => new ViewConsumableItem
+                {
+                    ConsumableItemID = ci.ConsumableItemID,
+                    ItemID = ci.ItemID,
+                    HealthValue = ci.HealthValue,
+                    EnergyValue = ci.EnergyValue,
+                    RadiationValue = ci.RadiationValue,
+                    Item = new ViewItem
+                    {
+                        ItemID = ci.Item.ItemID,
+                        Name = ci.Item.Name,
+                        TradeValue = ci.Item.TradeValue,
+                        Stackable = ci.Item.Stackable,
+                        Category = new ViewItemCategory
+                        {
+                            CategoryID = ci.Item.CategoryId,
+                            Name = ci.Item.Name,
+                        }
+                    }
+                })
+                .ToList();
+
+            return Ok(consumableItems);
 
         }
 
         [HttpGet("{id}")]
-        public ActionResult<DataConsumableItem> Get(int id)
+        public ActionResult<ViewConsumableItem> Get(int id)
         {
-            var consumableItem = _context.ConsumableItem.Find(id);
+            var consumableItem = _context.ConsumableItem
+                .Include(ci => ci.Item)
+                .ThenInclude(i => i.Category)
+                .FirstOrDefault(ci => ci.ItemID == id);
+
             if (consumableItem == null)
             {
                 return NotFound();
             }
-            return Ok(consumableItem);
+
+            return Ok(new ViewConsumableItem
+                {
+                    ConsumableItemID = consumableItem.ConsumableItemID,
+                    ItemID = consumableItem.ItemID,
+                    HealthValue = consumableItem.HealthValue,
+                    EnergyValue = consumableItem.EnergyValue,
+                    RadiationValue = consumableItem.RadiationValue,
+                    Item = new ViewItem
+                    {
+                        ItemID = consumableItem.Item.ItemID,
+                        Name = consumableItem.Item.Name,
+                        TradeValue = consumableItem.Item.TradeValue,
+                        Stackable = consumableItem.Item.Stackable,
+                        Category = new ViewItemCategory
+                        {
+                            CategoryID = consumableItem.Item.CategoryId,
+                            Name = consumableItem.Item.Name,
+                        }
+                    }
+                }
+            );
         }
 
         [HttpPost]
--- GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
+++ GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db0dba45cdbfa3ffca06c177d9cae252fbdd6e34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fdeca56a5714a4c3977eb3bd26a51dc3bce9006")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
