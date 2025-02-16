243148fe6197403e1228d00df871c33e9f4d6a5d
Jakub Šajer <jakub.sajer@gmail.com>
2025-02-16 22:31:59 +0100

    uploads endpoint, converted rest of files to .webp

--- GameBook.Server/Program.cs
+++ GameBook.Server/Program.cs
@@ -34,7 +34,6 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 app.UseDefaultFiles();
-app.UseStaticFiles();
 
 //Swagger
 if (app.Environment.IsDevelopment())
--- GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
+++ GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79edc589587b8369b5049674700d62e3777b5e85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+904d1a2f8e94c92ffef2b8ce84efdaec3a72a854")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
