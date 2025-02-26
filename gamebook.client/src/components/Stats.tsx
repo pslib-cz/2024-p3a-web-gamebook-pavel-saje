ff1f1a21fb12747b36980b17186acc7f5600e7b2
Jakub Šajer <jakub.sajer@gmail.com>
2025-02-26 19:05:56 +0100

    options menu styled

--- gamebook.client/src/components/Stats.tsx
+++ gamebook.client/src/components/Stats.tsx
@@ -1,29 +1,28 @@
-import React, { useContext } from 'react';
-import { GameContext } from '../context/GameContext';
+import React, { useContext } from "react";
+import { GameContext } from "../context/GameContext";
 
-import styles from '../styles/stats.module.css';
+import styles from "../styles/stats.module.css";
 
 const Stats: React.FC = () => {
-  const gameContext = useContext(GameContext);
+    const gameContext = useContext(GameContext);
 
-  if (!gameContext) {
-    return <div>Error: Game context is not available.</div>;
-  }
+    if (!gameContext) {
+        return <div>Error: Game context is not available.</div>;
+    }
 
-  const { hp } = gameContext;
-  const { energy } = gameContext;
+    const { hp } = gameContext;
+    const { energy } = gameContext;
     const { radiation } = gameContext;
     const { money } = gameContext;
 
-  return (
-    <div className={styles.stats}>
-      <p className={styles.hp}>♥️ {hp}</p>
-      <p className={styles.energy}>⚡ {energy}</p>
-      <p className={styles.radiation}>☢️ {radiation}</p>
-      <p className={styles.money}>💰 {money}</p>
-      {/* <button onClick={() => gameContext.setEnergy(energy - 10)}>-10⚡</button> */}
-    </div>
-  );
+    return (
+        <div className={styles.stats}>
+            <p className={styles.hp}>♥️ {hp}</p>
+            <p className={styles.energy}>⚡ {energy}</p>
+            <p className={styles.radiation}>☢️ {radiation}</p>
+            <p className={styles.money}>💲 {money}</p>
+        </div>
+    );
 };
 
 export default Stats;
