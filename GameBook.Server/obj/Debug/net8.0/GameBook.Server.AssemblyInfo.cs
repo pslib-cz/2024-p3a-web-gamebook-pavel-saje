c37584421ad78a3caf26b70ecb441d8c200f5ea4
L0SPAVL0S <pavel.horacek.022@pslib.cz>
2025-02-27 14:13:29 +0100

    pull

--- GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
+++ GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8ef2e1dd88571191ab8f557c8c03709269754f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+963c6d3bdc979edf10df80ce19f39609b082565a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
