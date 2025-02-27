b51626c9e914740091183088aca8ce528b667460
Jakub Šajer <jakub.sajer@gmail.com>
2025-02-27 16:29:27 +0100

    uwu mapa

--- GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
+++ GameBook.Server/obj/Debug/net8.0/GameBook.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d8ef2e1dd88571191ab8f557c8c03709269754f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48b55ffde2dfba595dca7b65bfa5b1ad3c30a138")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameBook.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
