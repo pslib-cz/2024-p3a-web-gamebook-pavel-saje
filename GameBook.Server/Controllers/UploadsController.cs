063aa674202498bf0665c7ccff5374fcade00339
L0SPAVL0S <pavel.horacek.022@pslib.cz>
2025-04-04 10:19:24 +0200

    píčovina5

--- GameBook.Server/Controllers/UploadsController.cs
+++ GameBook.Server/Controllers/UploadsController.cs
@@ -13,18 +13,14 @@ namespace GameBook.Server.Controllers
         {
             if (path == null) return BadRequest("Empty file path");
 
-            path = path.Replace("%5C", "/");
+            path = "/Uploads/" + path.Replace("%5C", "/");
             
-            if (!path.StartsWith("Uploads/"))
-            {
-                path = "Uploads/" + path;
-            }
-
             var webRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
             var uploads = Path.Combine(webRootPath, "Uploads");
 
             var filePath = Path.Combine(uploads, path);
             var fullPath = Path.GetFullPath(filePath);
+            fullPath = fullPath.Replace("%5C", "/");
 
             if (!fullPath.StartsWith(Path.GetFullPath(uploads)))
             {
